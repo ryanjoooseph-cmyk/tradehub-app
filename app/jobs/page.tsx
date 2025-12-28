6ddb32b7dc9587638c2142d274390c7d3bde6c68
ryanjoooseph-cmyk <ryan.joooseph@gmail.com>
2025-12-28 22:36:38 +1000

    agent: add Jobs page stub (app/jobs/page.tsx)

---
+++ app/jobs/page.tsx
@@ -0,0 +1,8 @@
+export default function JobsPage() {
+  return (
+    <main style={ padding: 24 }>
+      <h1>Jobs</h1>
+      <p>Stub page created by TradeHub agents on 2025-12-28 12:36:37.</p>
+    </main>
+  );
+}
