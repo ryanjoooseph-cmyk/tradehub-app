38ab3a730ad3b621ae28c27ccd47ccc0a904ca93
ryanjoooseph-cmyk <ryan.joooseph@gmail.com>
2025-12-29 09:31:04 +0800

    Revert "agent: introduce Jobs page stub"

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -1,8 +1,8 @@
 export default function JobsPage() {
   return (
-    <main style={{ padding: 24 }}>
+    <main style={ padding: 24 }>
       <h1>Jobs</h1>
-      <p>This page was scaffolded by TradeHub agents.</p>
+      <p>Stub page created by TradeHub agents on 2025-12-28 12:48:25.</p>
     </main>
   );
 }
