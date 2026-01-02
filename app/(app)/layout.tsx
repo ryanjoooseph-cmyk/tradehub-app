1e5691e77bcc852b445ee6edd66a314b401cadd6
ryanjoooseph-cmyk <ryan.joooseph@gmail.com>
2026-01-02 17:08:50 +0800

    Delete app/(app)/layout.tsx

--- app/(app)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import './styles.css';
-import AppShell from '@/components/AppShell';
-import { ReactNode } from 'react';
-
-export const metadata = { title: 'TradeHub' };
-
-export default function Layout({ children }: { children: ReactNode }) {
-  return <AppShell>{children}</AppShell>;
-}
