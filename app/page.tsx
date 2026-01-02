dac171fdae33395f31125c5aa251e0b1d380fd57
ryanjoooseph-cmyk <ryan.joooseph@gmail.com>
2026-01-02 16:39:28 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main style={{ padding: 24 }}>
-      <h1>TradeHub</h1>
-      <p><a href="/jobs">Jobs</a></p>
-    </main>
+    <div>
+      <h1>Welcome to TradeHub</h1>
+      <p>Start with a module:</p>
+      <ul>
+        <li><Link href="/jobs">Jobs</Link></li>
+        <li><Link href="/market">Market</Link></li>
+      </ul>
+    </div>
   );
 }
