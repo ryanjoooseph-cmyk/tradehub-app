002d870b0ad909f5da6b52b012713942c16ce2cf
ryanjoooseph-cmyk <ryan.joooseph@gmail.com>
2026-01-02 16:24:46 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      a: process.cwd() // enables imports like: a/lib/getBaseUrl
+    };
+    return config;
+  }
+};
+
 export default nextConfig;
