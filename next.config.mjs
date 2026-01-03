154ca6e0fa5c567e72f38e434a5c173502681025
ryanjoooseph-cmyk <ryan.joooseph@gmail.com>
2026-01-03 11:50:31 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  experimental: { esmExternals: true }
 };
 export default nextConfig;
