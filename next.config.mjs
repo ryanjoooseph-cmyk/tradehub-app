db8b2c130821a5eded7333fe7ea2ca20eb2176a0
ryanjoooseph-cmyk <ryan.joooseph@gmail.com>
2026-01-03 10:30:04 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: false }
+  reactStrictMode: false
 };
-
 export default nextConfig;
