c0e9a942e861216d1edb2575e7a78be31beb26e5
ryanjoooseph-cmyk <ryan.joooseph@gmail.com>
2026-01-06 16:42:18 +0800

    Delete lib directory

--- lib/getBaseUrl.ts
+++
@@ -1,7 +0,0 @@
-export function getBaseUrl() {
-  return (
-    process.env.NEXT_PUBLIC_BASE_URL ||
-    process.env.RENDER_EXTERNAL_URL ||
-    'http://localhost:3000'
-  );
-}
--- lib/supabaseAdmin.ts
+++
@@ -1,8 +0,0 @@
-// lib/supabaseAdmin.ts
-import { createClient } from "@supabase/supabase-js";
-
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const service = process.env.SUPABASE_SERVICE_ROLE!;
-
-const supabaseAdmin = createClient(url, service, { auth: { persistSession: false } });
-export default supabaseAdmin;
