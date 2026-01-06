a660a710d8989fba739334928a6289b29f26f1ba
ryanjoooseph-cmyk <ryan.joooseph@gmail.com>
2026-01-06 16:42:11 +0800

    Delete components directory

--- components/AppShell.tsx
+++
@@ -1,6 +0,0 @@
-// components/AppShell.tsx
-import React from "react";
-
-export default function AppShell({ children }: { children: React.ReactNode }) {
-  return <div className="app-shell">{children}</div>;
-}
