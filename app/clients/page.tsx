aff0838249133d6b5e66db35988c015f094e7421
ryanjoooseph-cmyk <ryan.joooseph@gmail.com>
2026-01-07 15:58:23 +0800

    Update page.tsx

--- app/clients/page.tsx
+++ app/clients/page.tsx
@@ -1,7 +1,3 @@
 export default function Page() {
-  return (
-    <div style={{ fontSize: 20, padding: 16 }}>
-      Clients
-    </div>
-  );
+  return <div style={{ fontSize: 20, padding: 16 }}>Clients</div>;
 }
