a52b5b86a3e1c5153dcc907d2d832d762767c495
ryanjoooseph-cmyk <ryan.joooseph@gmail.com>
2026-01-09 14:36:56 +0800

    Update page.tsx

--- app/tradehub/jobs/calendar/page.tsx
+++ app/tradehub/jobs/calendar/page.tsx
@@ -1,8 +1,8 @@
-import Scheduler from "../../../../components/Scheduler";
+import Scheduler from '@/components/Scheduler';
 
 export default function CalendarPage() {
   return (
-    <div className="p-6">
+    <div className="px-6">
       <h1 className="text-xl font-semibold mb-4">Jobs Calendar</h1>
       <Scheduler />
     </div>
