dff465755062a8b7cedff31255cecf2bc3f83e07
RevStreamAi <ryan@revstreamai.com>
2026-02-01 20:46:26 +1100

    Fix lint/typecheck config and stabilize Next 16 build path

--- web/src/app/app/layout.tsx
+++ web/src/app/app/layout.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic"
+export const revalidate = 0
+
 import AppShell from "@/components/app/AppShell";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
