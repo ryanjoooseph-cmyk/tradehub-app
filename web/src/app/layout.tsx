d274585f65418d41c5d8666e2671673acc4fde06
RevStreamAi <ryan@revstreamai.com>
2026-02-22 14:07:52 +1100

    PR21: revert root layout change (avoid high-risk PR)

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -3,16 +3,12 @@ export const revalidate = 0
 
 import "./globals.css";
 import Providers from "./providers";
-import { CommandPalette } from "@/components/CommandPalette";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <Providers>
-          {children}
-          <CommandPalette />
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
